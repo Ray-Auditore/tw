64bf544dd9f257e0f30946da814f30ae693e5334
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-02-21 10:59:35 +0700

    penembahan darkmode
    penambahan darkmode di web

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        kucing: 'url("../public/image/kucing.jpeg")',
-        hutan: 'url("../public/image/forest.png")',
-        pantai: 'url("../public/image/pantai.jpeg")',
-        art: 'url("../public/image/art.jpg")',
-        vlue: 'url("../public/image/127.jpg")',
-
+  darkMode: "class",
+  transitionDelay: {
+    2000: "2000ms",
+    theme: {
+      extend: {
+        backgroundImage: {
+          kucing: 'url("../public/image/kucing.jpeg")',
+          hutan: 'url("../public/image/forest.png")',
+          pantai: 'url("../public/image/pantai.jpeg")',
+          art: 'url("../public/image/art.jpg")',
+          vlue: 'url("../public/image/127.jpg")',
+        },
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
