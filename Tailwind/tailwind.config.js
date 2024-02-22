ed20ebbe7dc6d00bf255ccc5a87f3ea3ccf2acec
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-02-22 13:55:27 +0700

    Tugas detail info
    membikin detail info produk

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -2,19 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   darkMode: "class",
-  transitionDelay: {
-    2000: "2000ms",
-    theme: {
-      extend: {
-        backgroundImage: {
-          kucing: 'url("../public/image/kucing.jpeg")',
-          hutan: 'url("../public/image/forest.png")',
-          pantai: 'url("../public/image/pantai.jpeg")',
-          art: 'url("../public/image/art.jpg")',
-          vlue: 'url("../public/image/127.jpg")',
-        },
+  theme: {
+    extend: {
+      backgroundImage: {
+        kucing: 'url("./public/image/kucing.jpeg")',
+        hutan: 'url("../public/image/forest.png")',
+        pantai: 'url("../public/image/pantai.jpeg")',
+        art: 'url("../public/image/art.jpg")',
+        vlue: 'url("../public/image/127.jpg")',
+        onit: 'url("../public/image/sepatu.jpeg")',
       },
     },
+    // },
     plugins: [],
   },
 };
